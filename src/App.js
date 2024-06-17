2ce55c2db6d11e8c18eafade663b7827c488ef4c
arwa mahmoud <Q-arwa>
2024-06-17 12:44:04 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
+import { Routes ,Route} from 'react-router-dom';
+import Movies from './Components/Movies';
+import TV from './Components/TV';
+import Person from './Components/Person';
+import Home from './Components/Home';
+import Navbar from './Components/Navbar';
+import NotFound from './Components/NotFound';
+import MoviesDeatils from './Components/MoviesDeatils';
+
+
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+
+<Navbar/>
+    <div className='container'>
+   
+<Routes>
+<Route path='/' element={<Home/>}/>
+  
+  <Route path='home' element={<Home/>}/>
+  <Route path='movies' element={<Movies/>}/>
+  <Route path='tvshow' element={<TV/>}/>
+  <Route path='person' element={<Person/>}/>
+  <Route path='movieDetails/:id' element={<MoviesDeatils/>}/>
+  <Route path='*' element={<NotFound/>}/>
+    </Routes>
     </div>
+
+   
+    
+    </>
   );
 }
 
